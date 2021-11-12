0274caf28ec8783f5cc692ba4357d1d8b19b628b
RetiredDairyCow <atg.ashesh@gmail.com>
2021-11-12 15:05:22 -0800

    p3b phonebook commit before heroku

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 /* const { request, response } = require('express' )*/
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 app.use(express.json())
-
+app.use(cors())
 
 morgan.token('type', function (req, res) { 
     if (req.method === 'POST'){
@@ -85,7 +86,7 @@ app.post('/api/persons', (request, response) => {
 
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`)
 })
