7ea433dcfbcd2e3bf544b717927a2cae20d645a2
RetiredDairyCow <atg.ashesh@gmail.com>
2021-11-13 13:31:57 -0800

    uibuild

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 const app = express()
 app.use(express.json())
 app.use(cors())
+app.use(express.static('build'))
 
 morgan.token('type', function (req, res) { 
     if (req.method === 'POST'){
